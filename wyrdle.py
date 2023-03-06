f32be9ab67e9deab3a7d47dcd6b4f25c03393795
huntmckay <huntmckay@gmail.com>
2023-03-05 22:22:22 -0500

    1.0.0 fully functional

--- wyrdle.py
+++ wyrdle.py
@@ -30,7 +30,7 @@ def get_random_word(wordlist):
 
     return random.choice(words)
     
-def show_guess(guess,word):
+def show_guess(guesses,word):
     """Show the user's guess on the terminal and classify all letters.
 
     ## Example:
@@ -40,42 +40,72 @@ def show_guess(guess,word):
     Misplaced letters: N
     Wrong letters: C, R
     """
+    for guess in guesses:
+        styled_guess = []
+        for letter, correct  in zip(guess, word):
+            if letter == correct:
+                style = "bold white on green"
+            elif letter in word:
+                style = "bold white on yellow"
+            elif letter in ascii_letters:
+                style = "white on #666666"
+            else:
+                style = "dim"
+            styled_guess.append(f"[{style}]{letter}[/]")
 
-    if guess == word:
-        return True
-    else:
-        correct_letters = { letter for letter, correct in zip(guess, word) if letter == correct }
-        misplaced_letters = set(guess) & set(word) - correct_letters
-        wrong_letters = set(guess) - set(word)
 
-        print("Correct letters:", ", ".join(sorted(correct_letters)))
-        print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
-        print("Wrong letters:", ", ".join(sorted(wrong_letters)))
+        console.print("".join(styled_guess), justify="center")
+    # if guess == word:
+    #     return True
+    # else:
+    #     correct_letters = { letter for letter, correct in zip(guess, word) if letter == correct }
+    #     misplaced_letters = set(guess) & set(word) - correct_letters
+    #     wrong_letters = set(guess) - set(word)
+
+    #     print("Correct letters:", ", ".join(sorted(correct_letters)))
+    #     print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
+    #     print("Wrong letters:", ", ".join(sorted(wrong_letters)))
         
-def end_game(word,win):
+def end_game(guesses, word, guessed_correctly=False):
     
-    if win:
-        print(f"CORRECT! The Secret word was {word}")
+    refresh_page(headline="Game Over")
+    show_guess(guesses, word)
+
+    if guessed_correctly:
+        console.print(f"\n[bold white on green]Correct, the word is {word}[/]")
     else:
-        print(f"LOSER! The Secret word was {word}")
+        console.print(f"\n[bold white on red]Sorry, the word is {word}[/]")
+    # if win:
+    #     print(f"CORRECT! The Secret word was {word}")
+    # else:
+    #     print(f"LOSER! The Secret word was {word}")
 
 def main():
     
     #Pre-Process
     word_path = pathlib.Path(__file__).parent / "wordlist.txt"
     word = get_random_word(word_path.read_text(encoding="utf-8").split("\n")) 
-    print(f"Secret word is {word}")
+    guesses = ["_" * 5] * 6
 
     #Main Process
-    for guess_count in range(1,7):
-        guess = input(f"\nGuess {guess_count}: ").upper()
-        if show_guess(guess,word):
-            end_game(word,win=True)
-            break
+    guessed_correctly = False
+    for guess_count in range(6):
 
+        refresh_page(headline=f"Guess {guess_count + 1}")
+        show_guess(guesses, word) 
+        print(f"Secret word is {word}")
+
+        guesses[guess_count] = input("\nGuess word: ").upper() 
+        if guesses[guess_count] == word:
+            guessed_correctly = True
+            break
     #Post-Process
-    else:
-        end_game(word,win=False)
+    end_game(guesses, word, guessed_correctly)
+
+def valid_input(guess):
+    for i in guess:
+        if ord(i.lower()) in range(97,123):
+            return True
 
 if __name__ == '__main__':
     main()
