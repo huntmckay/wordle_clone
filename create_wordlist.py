766b24339886a29fdc5bc14f9da3e45f5cd781ee
huntmckay <huntmckay@gmail.com>
2023-03-05 22:53:08 -0500

    create wordlist updated with doctest

--- create_wordlist.py
+++ create_wordlist.py
@@ -1,4 +1,4 @@
-# create_wordlist.py
+# setup_wordlist.py
 
 import pathlib
 import sys
@@ -7,13 +7,41 @@ from string import ascii_letters
 in_path = pathlib.Path(sys.argv[1])
 out_path = pathlib.Path(sys.argv[2])
 
-words = sorted(
-    {
-        word.lower()
-        for word in in_path.read_text(encoding="utf-8").split()
-        if all(letter in ascii_letters for letter in word) and len(word) == 5
-    },
-    key = lambda word: (len(word), word),
-)
-out_path.write_text("\n".join(words))
+"""
+*in_path* supplies create_wordlist() with a plaintext file
+*out_path* is the output filename for a wordlist that setup will populate with 5 letter words
 
+## Example
+
+>>> in_path = pathlib.Path('test_input.txt')
+>>> out_path = pathlib.Path('test_output.txt')
+>>> create_wordlist(in_path,out_path)
+
+These are the 5 length words pulled from test_input.txt
+beats
+break
+cases
+first
+great
+never
+often
+there
+"""
+
+def create_wordlist(in_file, outfile):
+
+    words = sorted(
+        {
+            word.lower()
+            for word in in_path.read_text(encoding="utf-8").split()
+            if all(letter in ascii_letters for letter in word) and len(word) == 5
+        },
+        key = lambda word: (len(word), word),
+    )
+    print(f"These are the 5 length words pulled from {in_path}")
+    print("\n".join(words))
+
+    out_path.write_text("\n".join(words))
+
+if __name__ == '__main__':
+    create_wordlist(in_path, out_path)
